76437d04526736fcb528901a6d4e763f2e3db4b7
Andrew Bose <aljbose@gmail.com>
2014-01-31 13:07:13 -0600

    Adding all files.

---
+++ SplashScreen/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="988fa52e-f4da-457b-b6b6-b61ac4a2a14a" description="SharePoint App Feature" featureId="988fa52e-f4da-457b-b6b6-b61ac4a2a14a" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="SplashScreen Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="e8ff4dd0-b013-4f94-99ee-86a7246e2485" />
+    <projectItemReference itemId="2ac202bc-9209-4cb1-8cd4-1aa3cb1ed7c3" />
+    <projectItemReference itemId="6a5614ca-05cb-4fa9-875c-7e37e285691b" />
+    <projectItemReference itemId="fbd6eba1-6002-4daa-94f5-ab2370e869fa" />
+  </projectItems>
+</feature>
---
+++ SplashScreen/Scripts/App.js
@@ -0,0 +1,45 @@
+﻿'use strict';
+
+var context = SP.ClientContext.get_current();
+var user = context.get_web().get_currentUser();
+
+$(document).ready(function () {
+    // Hide the globalNavBox element if IsDlg querystring parameter is set
+    //var isDialog = getQueryStringParameterByName('IsDlg');
+    //if (isDialog == '1') {
+        //$("#globalNavBox").hide();
+    //}
+
+    getUserName();
+});
+
+function getUserName() {
+    context.load(user);
+    context.executeQueryAsync(onGetUserNameSuccess, onGetUserNameFail);
+}
+
+
+function onGetUserNameSuccess() {
+    if (null != user) {
+        //The user is not null
+        var userName = null;
+        try {
+            userName = user.get_title();
+        } catch (e) {
+            userName = "Anonymous user!";
+        }
+    }
+    $('#message').text('User Name: ' + userName);
+}
+
+
+function onGetUserNameFail(sender, args) {
+    $('#message').text('Failed to get user name. Error:' + args.get_message());
+}
+
+// This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model
+ 
+function getQueryStringParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
